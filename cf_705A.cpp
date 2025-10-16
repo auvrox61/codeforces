9c7531de870c7813f3529db06c227bab65cc8491
auvrox61 <auvro61@gmail.com>
2025-10-16 23:26:36 +0600

    Add files via upload

---
+++ cf_705A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        if(i%2!=0){
+            cout<<"I love"<<" ";
+        } else{
+            cout<<"I hate"<<" ";
+        }
+        if(i==n-1){
+            cout<<"it";
+        } else{
+            cout<<"that"<<" ";
+        }
+    }
+    return 0;
+}
