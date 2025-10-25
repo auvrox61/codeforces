63f407aaaedffb91f1870fe7322caf0384b23ebd
auvrox61 <auvro61@gmail.com>
2025-10-25 21:06:13 +0600

    Add files via upload

---
+++ cf_510A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            for (int j = 0; j < m; j++) cout << "#";
+        } else {
+            // Even row
+            if ((i / 2) % 2 != 0) {
+                for (int j = 0; j < m - 1; j++) cout << ".";
+                cout << "#";
+            } else {
+                cout << "#";
+                for (int j = 0; j < m - 1; j++) cout << ".";
+            }
+        }
+        cout << endl; 
+    }
+    
+    return 0;
+}
